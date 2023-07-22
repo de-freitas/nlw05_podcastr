62ec08b71a39402c5ebe924f77320fdbe90eb85f
Claudio Gabriel Queiroz de Freitas <claudio.freitas@ayoshii.com.br>
2023-07-22 18:58:59 -0300

    Definidos estilos globais da aplicação no arquivo global.scss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// neste arquivo é possível configurar o HTML que fica por volta de toda a aplicação
+// fontes, por exemplo, devem ser configuradas aqui. Ao trocar de rota, a fonte não precisa ser carregada novamente
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
