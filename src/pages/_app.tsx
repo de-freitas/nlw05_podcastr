007d6948f76bf9e4a92bd569351b41cb6f59ea6b
Claudio Gabriel Queiroz de Freitas <claudio.freitas@ayoshii.com.br>
2023-07-22 21:21:32 -0300

    criadas as partes visuais dos componentes Header e Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import type { AppProps } from "next/app";
 import "../styles/global.scss";
 
+import type { AppProps } from "next/app";
+
+import { Header } from "../components/Header/Header";
+import { Player } from "../components/Player/Player";
+import styles from "../styles/app.module.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
+
+// export default function App() {
+//   return (
+//     <>
+//       <Header />
+//     </>
+//   );
+// }
